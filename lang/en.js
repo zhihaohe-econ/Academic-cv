53d7f7dccad0b38f26112186f8afb2f9551b5541
Zhihao <hezhihao@ruc.edu.cn>
2026-01-07 07:59:28 +0800

    Update en.js

--- lang/en.js
+++ lang/en.js
@@ -15,12 +15,10 @@ const enHomePageData = {
   home_title: "About me",
   home_content: `
   <div>
-    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <p> Hello, I'm Zhihao He, a dual Ph.D. candidate at School of Public Administration and Policy & College of Business, Renmin University of China & City University of Hong Kong.
     </p>
-    <h2 class='title'> Main works </h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh  </p>
+    <h2 class='title'> Research interests </h2>
+    <p>Urban Economics; Real Estate Economics; Land Economics. </p>
     <ul>
       <li> Heuristics</li>
       <li> Policy planning </li>
