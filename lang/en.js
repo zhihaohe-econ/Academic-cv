d78c29f183d16033cf3e67f5d6b7b13ccc126fc2
Zhihao <hezhihao@ruc.edu.cn>
2026-01-08 11:04:51 +0800

    Update en.js

--- lang/en.js
+++ lang/en.js
@@ -234,6 +234,21 @@ const enCVPageData = {
         },
       ]
     },
+        {
+      title: "Researching Experiences",
+      items: [
+       {
+          role: "RA to Prof. Charles Leung",
+          institution: "College of Business, City University of Hong Kong",
+          date: "11.2025-12.2025"
+        },
+        {
+          role: "RA to Prof. Yinghao Pan",
+          institution: "National Academy of Development and Strategy, Renmin University of China",
+          date: "09.2023-Present"
+        },
+      ]
+    },
     {
       title: "Professional Services",
       items: [
